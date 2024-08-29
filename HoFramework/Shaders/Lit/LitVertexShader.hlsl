6c6cd0b26f028238b6fa31ea5dd7bc7e7601d9d2
NavySunKOR <dragunov2008@naver.com>
2024-08-29 17:53:24 +0900

    기존 DragonPixelShader와 VertexShader를 복사하여 새로 LitPixelShader.hlsl LitVertexShader.hlsl로 생성

---
+++ HoFramework/Shaders/Lit/LitVertexShader.hlsl
@@ -0,0 +1,22 @@
+#include "../Cores/Core.hlsli"
+#include "../Cores/VSHeader.hlsli"
+
+PSInput main(VSInput Input)
+{
+    PSInput Output;
+    float4 Position = float4(Input.Position, 1.f);
+
+    Position = mul(Position, ModelTransform);
+    Output.WorldPosition = Position.xyz;
+
+    Position = mul(Position, ViewTransform);
+    Position = mul(Position, ProjectionTransform);
+    Output.ScreenPosition = Position;
+
+    Output.Color = Input.Color;
+    Output.TexCoord = Input.TexCoord;
+    Output.Normal = mul(float4(Input.Normal, 0), InverseTransform).xyz;
+    Output.Normal = normalize(Output.Normal);
+
+    return Output;
+}
